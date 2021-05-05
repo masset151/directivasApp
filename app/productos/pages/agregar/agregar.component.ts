0202a0dd2726ea4202de16bff1a39be1f1c01965
masset151 <masset.ruiz151@gmail.com>
2021-05-05 18:16:13 +0200

    formulario reactivo

--- app/productos/pages/agregar/agregar.component.ts
+++ app/productos/pages/agregar/agregar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
   styleUrls: ['./agregar.component.css']
 })
-export class AgregarComponent implements OnInit {
+export class AgregarComponent {
 
-  constructor() { }
+  miFormulario:FormGroup = this.fb.group({
+    nombre:['',Validators.required]
+  });
+  constructor(private fb:FormBuilder) { }
 
-  ngOnInit(): void {
+  tieneError(campo:string):boolean{
+    return this.miFormulario.get(campo)?.invalid || false
   }
 
 }
--- app/productos/productos.module.ts
+++ app/productos/productos.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductosRoutingModule } from './productos-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
+
 
 
 @NgModule({
   declarations: [AgregarComponent],
   imports: [
     CommonModule,
-    ProductosRoutingModule
+    ProductosRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductosModule { }
